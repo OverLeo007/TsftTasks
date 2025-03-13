0c48772ffc0f2e30760e3e7343bfa5cf5c15eaa6
Lev Sokolov <sokolov.lev.dp@gmail.com>
2025-03-13 23:19:27 +0700

    Variant 1: x2 rows, counting line row in for cycle

--- task1/src/main/java/ru/shift/Main.java
+++ task1/src/main/java/ru/shift/Main.java
@@ -1,17 +1,65 @@
 package ru.shift;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final String VERTICAL_DELIMITER = "|";
+    private static final String HORIZONTAL_DELIMITER = "-";
+    private static final String CROSS_DELIMITER = "+";
+    private static final String SPACE = " ";
+    private static final String EOL = System.lineSeparator();
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number of rows from 1 to 32:");
+        var a = scanInt();
+        var maxLen = String.valueOf(a * a).length();
+        var maxLenFirstRow = String.valueOf(a).length();
+        var n = 2 * (a + 1);
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < a + 1; j++) {
+                var value = getElementValue(i, j, maxLen, maxLenFirstRow);
+                var delimiter = getDelimiter(i, j, a - 1);
+                System.out.print(value + delimiter);
+            }
         }
+    }
+
+    private static String getElementValue(int i, int j, int maxLen, int maxLenFirstRow) {
+        var elemLen = j == 0 ? maxLenFirstRow : maxLen;
+        if (i == 0 && j == 0) {
+            return SPACE.repeat(elemLen);
+        } else if (i % 2 == 0) {
+            var i_val = i / 2;
+            var value = (i == 0 || j == 0) ? i_val + j : i_val * j;
+            return String.format("%" + elemLen + "d", value);
+        } else {
+            return HORIZONTAL_DELIMITER.repeat(elemLen);
+        }
+    }
+
+    private static String getDelimiter(int i, int j, int n) {
+        if (j == n + 1) {
+            return EOL;
+        }
+        return i % 2 == 0 ? VERTICAL_DELIMITER : CROSS_DELIMITER;
+    }
+
+    private static int scanInt() {
+        var sc = new Scanner(System.in);
+        int value;
+        while (true) {
+            var input = sc.nextLine();
+            try {
+                value = Integer.parseInt(input.trim());
+                if (value >= 1 && value <= 32) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter an integer from 1 to 32.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter an integer from 1 to 32.");
+            }
+        }
+        return value;
     }
 }
